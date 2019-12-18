9164490d657d9af9a0bcd23d37c03c59c1267aac
luddilo <ludvig@furhatrobotics.com>
2019-12-18 10:17:00 +0100

    Updated to 0.1.0

--- flightbooker/src/agent.ts
+++ flightbooker/src/agent.ts
@@ -8,7 +8,7 @@ const agent: Agent = {
     narrative,
     questions,
     bridges: ["So", "Where were we", "Now"],
-    narratoryKey: "ENTER-NARRATORY-KEY-HERE", // Enter your Narratory key here
+    narratoryKey: "ENTER-NARRATORY-KEY-HERE", // Enter your Narratory key here, sign up att narratory.io if you don't have one
     googleCredentials: require("../google_credentials.json") // Populate this file, or change the link to your existing credentials file
 }
 
